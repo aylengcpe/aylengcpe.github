02285ca66f97a2c830cc77d4cd81c156cdd88868
Harold Abejaron <habejaron@gmail.com>
2020-01-11 14:25:49 +0800

    Function Number 0 added

--- Calculator/Calculator/Form1.vb
+++ Calculator/Calculator/Form1.vb
@@ -24,7 +24,12 @@
     End Sub
 
     Private Sub Button31_Click(sender As Object, e As EventArgs) Handles Button31.Click
+        If screen.Text = "0" Then
+            screen.Text = "0"
+        Else
+            screen.Text = screen.Text & "0"
 
+        End If
     End Sub
 
     Private Sub Button25_Click(sender As Object, e As EventArgs) Handles Button25.Click
